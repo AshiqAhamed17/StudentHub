aaac479b50b3eac983e4a58168ecd9baa36a892a
Ashiq Ahamed <aashiq121205@gmail.com>
2024-10-08 15:32:19 +0530

    basic file Struct

---
+++ main/src/assets/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    return (
+        <div>
+            <p> Home</p>
+            <p> Dashboard</p>
+            <p> Profile</p>
+        </div>
+    )
+}
+
+export default Navbar;
+
+
